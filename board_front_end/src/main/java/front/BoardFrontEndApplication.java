d494430fb072d0d913fead29ee7b9a6c3cd0040d
Admin <Admin@DESKTOP-EEKJFHH>
2025-04-15 17:56:04 +0900

    00

---
+++ board_front_end/src/main/java/front/BoardFrontEndApplication.java
@@ -0,0 +1,13 @@
+package front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardFrontEndApplication.class, args);
+	}
+
+}
