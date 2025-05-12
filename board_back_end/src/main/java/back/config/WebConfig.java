79be594cfe67dbbfb39cf35da97d81530bc26393
lee-eun-sik <dldmstlr20419@naver.com>
2025-05-13 02:11:12 +0900

    1

--- board_back_end/src/main/java/back/config/WebConfig.java
+++ board_back_end/src/main/java/back/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) { // 뭔가 디폴트한 값이 여기에 들어감
         registry.addMapping("/api/**") // 또는 "/**"주소를 갖고있는것이 오면, 프론트단 주소인지 확인
-                .allowedOriginPatterns("http://localhost:8080", "http://localhost:3000") // 프론트 주소, 주소로 오는지 확인
+                .allowedOriginPatterns("http://localhost:8081", "http://localhost:3000") // 프론트 주소, 주소로 오는지 확인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 통신 방식 열어줌
                 .allowedHeaders("*")
                 .allowCredentials(true); // 쿠키 허용!
