8e8a0829627c8d96348beb9ae59fe2cbaa75c644
leeeunsik <dldmstlr20419@naver.com>
2025-05-26 09:30:10 +0900

    00

--- project_back_end/src/main/java/back/service/auth/KakaoOAuthService.java
+++
@@ -1,9 +0,0 @@
-package back.service.auth;
-
-import back.model.user.User;
-
-public interface KakaoOAuthService {
-	User getUserInfo(String accessToken) throws Exception;
-    User processUser(User kakaoUser) throws Exception;
-    String createJwtToken(User user) throws Exception;
-}
